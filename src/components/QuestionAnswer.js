5b364f3970603c7a36eda1faf73cff75a10a4e21
Sudhanshu7sb <sudhanshu7.sb@gmail.com>
2020-09-24 01:03:28 +0530

    Add question answer section

---
+++ src/components/QuestionAnswer.js
@@ -0,0 +1,87 @@
+import React from "react";
+
+function QuestionAnswer() {
+  return (
+    <>
+      <section>
+        <article className="flex box mb-0 px-6">
+          <div className="product-details flex-center box">
+            <div className="about-product content">
+              <h5 className="is-medium mb-0"> Whats is your Question?</h5>
+              <p>
+                Since, I am a programmer, so the best site to find the answers
+                to my most of the questions is stack overflow. And believe me,
+                it's the best site till date for programmers. I can't think
+                about a day if this site shuts down. Nightmare!.
+              </p>
+
+              <div>
+                <a href="##" className="comment-box px-1 py-1 mr-2">
+                  like
+                  <i className="fas fa-comment mr-3"></i>
+                </a>
+              </div>
+            </div>
+            <div className="upvotes has-text-right">
+              <p className="has-text-black is-medium">
+                <a href="##">Read More&rarr;</a>
+              </p>
+            </div>
+          </div>
+        </article>
+        <article className="flex box mb-0 px-6">
+          <div className="product-details flex-center box">
+            <div className="about-product content">
+              <h5 className="is-medium mb-0"> Whats is your Question?</h5>
+              <p>
+                Since, I am a programmer, so the best site to find the answers
+                to my most of the questions is stack overflow. And believe me,
+                it's the best site till date for programmers. I can't think
+                about a day if this site shuts down. Nightmare!.
+              </p>
+
+              <div>
+                <a href="##" className="comment-box px-1 py-1 mr-2">
+                  like
+                  <i className="fas fa-comment mr-3"></i>
+                </a>
+              </div>
+            </div>
+            <div className="upvotes has-text-right">
+              <p className="has-text-black is-medium">
+                <a href="##">Read More&rarr;</a>
+              </p>
+            </div>
+          </div>
+        </article>
+        <article className="flex box mb-0 px-6">
+          <div className="product-details flex-center box">
+            <div className="about-product content">
+              <h5 className="is-medium mb-0"> Whats is your Question?</h5>
+              <p>
+                Since, I am a programmer, so the best site to find the answers
+                to my most of the questions is stack overflow. And believe me,
+                it's the best site till date for programmers. I can't think
+                about a day if this site shuts down. Nightmare!.
+              </p>
+
+              <div>
+                <a href="##" className="comment-box px-1 py-1 mr-2">
+                  like
+                  <i className="fas fa-comment mr-3"></i>
+                </a>
+              </div>
+            </div>
+            <div className="upvotes has-text-right">
+              <p className="has-text-black is-medium">
+                <a href="##">Read More&rarr;</a>
+              </p>
+            </div>
+          </div>
+        </article>
+      </section>
+    </>
+  );
+}
+
+export default QuestionAnswer;
--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import QuestionAnswer from "../components/QuestionAnswer";
 
 
 class App extends React.Component {
@@ -29,7 +30,7 @@ class App extends React.Component {
     return (
         <>
         <Header />
-        <h2>Forum</h2>
+        <QuestionAnswer />
         <Footer />
         </>
     )
